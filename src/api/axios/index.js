61aa1f898920b07f8dd2fe2524fb2a96012071d6
ABU TAHER <abutaher267@gmail.com>
2023-12-15 02:19:24 +0600

    update live api

--- src/api/axios/index.js
+++ src/api/axios/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const baseURL =
 import.meta.env.MODE === "development"
     ? "http://localhost:4001/api/v1"
-    : "https://jfksd/api/v1";
+    : "https://pz-attendance.onrender.com/api/v1";
 
 // export const baseURL = "https://api1.convertmybankstatement.com/api/v1";
 // const baseURL = "https://cmbs.onrender.com/api/v1";
