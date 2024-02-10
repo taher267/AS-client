d094e89eec8538fa4af837d341d8a7a31cbfac3c
ABU TAHER <abutaher267@gmail.com>
2024-02-10 12:47:51 +0600

    initial hide show ctrl

--- src/components/Layouts/Auth/index.jsx
+++ src/components/Layouts/Auth/index.jsx
@@ -22,7 +22,9 @@ const AuthLayout = () => {
 
     // Attach the event listener when component mounts
     window.addEventListener("resize", handleResize);
-
+    if (window.innerWidth < 768) {
+      setShowSidebar(false);
+    }
     // Detach the event listener when component unmounts
     return () => {
       window.removeEventListener("resize", handleResize);
